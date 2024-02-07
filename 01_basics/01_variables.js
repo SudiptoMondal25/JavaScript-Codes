928f2ddb96446c9915283c28aa5da02dc5f4c413
Sudipto Mondal <88684083+SudiptoMondal25@users.noreply.github.com>
2024-02-07 06:38:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const ID = "3237127"
+let email = "jhsj@gmail.com"
+/*
+DO NOT USE var to declare variables 
+because of issues in block scope and functional scope
+*/
+let password = "271123dfd"
+city = "Dhaka"
+let accState;
+
+console.log(ID);
+
+console.table([ID, email, password, city, accState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)   //we are using nodejs, not browser
+// code readability should be high
+
+console.log(3+3);
+
+console.log("hgsd");
+let age = 24
+let isLoggedIn = false
+let state = ""
+
+//number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => false/true
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof null);
