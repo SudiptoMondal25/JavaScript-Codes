887d43595b9bd3d45bdc9c8df08b5d1d1bb55d8c
Sudipto Mondal <88684083+SudiptoMondal25@users.noreply.github.com>
2024-02-07 07:36:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// true => 1 ; false => 0
+
+// let isLoggedIn = 0;
+let isLoggedIn = "Sudipto";
+let boolLoggedIn = Boolean(isLoggedIn)
+//console.log(boolLoggedIn);
+
+// true => 1 ; false => 0
+//"" => 0 ; "Something" => 1
+
+let number = 25
+let stringNum = String(number)
+console.log(stringNum);
+console.log(typeof stringNum);
