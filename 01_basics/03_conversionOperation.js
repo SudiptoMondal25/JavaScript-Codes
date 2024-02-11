0bed90485702275d5c81af0afc03ae88e1b86279
Sudipto Mondal <88684083+SudiptoMondal25@users.noreply.github.com>
2024-02-11 15:05:22 +0000

    datatype conversion final

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,40 @@ let boolLoggedIn = Boolean(isLoggedIn)
 
 let number = 25
 let stringNum = String(number)
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+//*********************** Operations **************************/
+
+let value = 5
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);      // 2 power of 2 (2^2)
+// console.log(2/2);      
+// console.log(2%2);   
+
+let str1 = "Hello"
+let str2 = " Sudipto"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);       //122 if string is at first then all will be converted to string
+// console.log(1 + 2 + "2");       //32 if number first then calculation will be done first then string will be added
+
+
+// console.log(true);       => true
+// console.log(+true);      => 1
+// console.log(+"");        => 0
+
+let gameCounter = 100
+gameCounter++       //prefix
+++gameCounter       //postfix
+console.log(gameCounter);
