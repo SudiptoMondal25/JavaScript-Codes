3c43a91e22608c82c940b7c68d2d02ef979d9108
Sudipto Mondal <88684083+SudiptoMondal25@users.noreply.github.com>
2024-02-12 06:11:29 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log(2 < "5");
+// console.log(2 < "05");
+
+
+// WE HAVE TO AVOID THESE KIND OF COMPARISONS
+console.log(null  > 0);
+console.log(null  == 0);
+console.log(null  >= 0);
+
+console.log(undefined == 0);
+console.log(undefined != 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+// === checks datatype also, doesn't convert datatype like ==
+console.log("2" === 2);
